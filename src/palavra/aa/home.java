82c61f55e1015c19fb91cc9e923622bbf9f9a4c9
nychavs <chavesnaiury@gmail.com>
2022-12-07 15:36:46 -0300

    a rasc with action listener

---
+++ src/palavra/aa/home.java
@@ -0,0 +1,23 @@
+package palavra.aa;
+
+import javax.swing.*;
+
+public class home {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
+    private JTextField textField15;
+    private JTextField textField16;
+
+}
